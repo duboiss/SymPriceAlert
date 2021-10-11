682c66bc43d063b58b92d20b5611aff522fb518e
Steven DUBOIS <git@duboiss.fr>
2021-10-11 21:27:16 +0200

    Use PHP 8 AsCommand attribute for app:prices:check command

--- src/Command/CheckPricesCommand.php
+++ src/Command/CheckPricesCommand.php
@@ -6,25 +6,21 @@ namespace App\Command;
 
 use App\Event\ProductParsedEvent;
 use App\Service\ProductService;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Finder\Finder;
 
+#[AsCommand('app:prices:check', description: 'Check all prices in urls provided by DataProducts directory')]
 class CheckPricesCommand extends Command
 {
-    protected static $defaultName = 'app:prices:check';
     private const PRODUCTS_PATH = __DIR__ . '/../DataProducts';
 
     public function __construct(private ProductService $productService, private EventDispatcherInterface $dispatcher)
     {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this->setDescription('Check all prices in urls provided by DataProducts directory');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
